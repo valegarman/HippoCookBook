1c80047dcb79e83735cdf4d1dddd19b7cbc3a9c1
valegarman <valegarman@gmail.com>
2022-03-09 14:56:29 -0500

    small fixes preprocessSession

--- session_managment/sessionBatchScript.m
+++ session_managment/sessionBatchScript.m
@@ -12,7 +12,7 @@ batch_preprocessSession('basepath','G:\data\fNkx11','cleanArtifacts',({65,[]}),'
 batch_sessionSummary('basepath','E:\data\fCck1','analogChannelsList',0,'digitalChannelsList',1);
 
 
-preprocessSession('basepath','G:\data\fNkx11\fNkx11_201030_sess12','cleanArtifacts',({65,1}),'analogChannelsList',65);
+preprocessSession('basepath','G:\data\fNkx11\fNkx11_201102_sess13','cleanArtifacts',({65,1}),'analogChannelsList',65);
 
 preprocessSession('basepath','D:\Dropbox\DATA\sharedRecordings\NewXmlAnimal\190222\rec1_220219_sess1');
 computeSessionSummary('basepath','E:\data\fCck1\fCck1_220216_sess13','analogChannelsList',0,'digitalChannelsList',1);
