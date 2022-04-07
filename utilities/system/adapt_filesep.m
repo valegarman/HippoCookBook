1f6115ce9e87e3b8b992a00c213c1f863d8ebcca
Manuel Valero García <manu@Manuels-MacBook-Pro.local>
2022-04-07 17:18:54 -0400

    adding adapt_filesep

---
+++ utilities/system/adapt_filesep.m
@@ -0,0 +1,13 @@
+function [file_path_out] = adapt_filesep(file_path_in)
+% adapt file path according to SO
+% Manu Valero 2022
+    switch computer
+        case 'MACI64'
+            file_path_in(strfind(file_path_in,'\'))='/';
+        case 'PCWIN64'
+            file_path_in(strfind(file_path_in,'/'))='\';
+        case 'GLNXA64'
+            file_path_in(strfind(file_path_in,'\'))='/';
+    end
+    file_path_out = file_path_in;
+end
