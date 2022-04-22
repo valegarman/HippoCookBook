962cade4630c8ee03b8e31e3c398b4520f9e5f74
valegarman <valegarman@gmail.com>
2022-04-22 18:34:54 -0400

    include stackSessionResult, working in loadProjectResults

--- analysis/general/loadProjectResults.m
+++ analysis/general/loadProjectResults.m
@@ -1,4 +1,4 @@
-function  loadProjectResults(varargin)
+function [results] =  loadProjectResults(varargin)
 
 %   Load and stack all results for a given project
 %
@@ -12,6 +12,8 @@ addParameter(p,'indexedProjects_path',[],@isstring);
 addParameter(p,'indexedProjects_name','indexedSessions',@isstring);
 addParameter(p,'data_path',database_path,@isstring);
 addParameter(p,'includeSpikes',true,@isstring);
+addParameter(p,'includeLFP',false,@isstring);
+addParameter(p,'analysis_project_path',[],@isfolder);
 
 parse(p,varargin{:});
 
@@ -19,6 +21,9 @@ project = p.Results.project;
 indexedProjects_path = p.Results.indexedProjects_path;
 indexedProjects_name = p.Results.indexedProjects_name;
 data_path = p.Results.data_path;
+includeSpikes = p.Results.includeSpikes;
+includeLFP = p.Results.includeLFP;
+analysis_project_path = p.Results.analysis_project_path;
 
 %% find indexed sessions
 if isempty(indexedProjects_name)
@@ -28,12 +33,15 @@ if isempty(indexedProjects_path)
     warning('Not included the path where the indexed Projects .mat variable is located. Trying to find it...');
     indexedProjects_path = fileparts(which([indexedProjects_name,'.mat']));
 end
+if isempty(analysis_project_path)
+    analysis_project_path = indexedProjects_path;
+end
 
 load([indexedProjects_path filesep indexedProjects_name]);
 
 sessionNames = fieldnames(allSessions);
 for ii = 1:length(sessionNames)
-    sessions.basepaths{ii} = [database_path filesep allSessions.(sessionNames{ii}).path];
+    sessions.basepaths{ii} = [data_path filesep allSessions.(sessionNames{ii}).path];
     sessions.project{ii} = allSessions.(sessionNames{ii}).project;
 end
 
@@ -50,15 +58,26 @@ if ~strcmpi(project,'Undefined')
         error('Project name not recognized!');
     end
 end
-fprintf(' Loading %3.i sessions... \n',length(sessions.basepaths)); %\n
+fprintf('Loading %3.i sessions... \n',length(sessions.basepaths)); %\n
 
 %% load cellexplorer results
 cell_metrics = loadCellMetricsBatch('basepaths',sessions.basepaths);
 
-%% collect data
+%% collect data per session
+projectSessionResults = [];
 for ii = 1:length(sessions.basepaths)
     fprintf(' > %3.i/%3.i session \n',ii, length(sessions.basepaths)); %\n
     cd(sessions.basepaths{ii});
+    
+    % get some useful fields
+    spikes = loadSpikes;
+    projectSessionResults.numcells(ii) = spikes.numcells;
+    
+    % spikes
+    if includeSpikes
+        projectSessionResults.spikes{ii} = spikes;
+    end
+    clear spikes
     
     % optogenetic responses
     targetFile = dir('*.optogeneticResponse.cellinfo.mat'); load(targetFile.name);
@@ -91,21 +110,32 @@ for ii = 1:length(sessions.basepaths)
     clear hgammaMod
     
     % spatial modulation
-    targetFile = dir('*placeFields.cellinfo.mat'); load(targetFile.name);
-    projectSessionResults.placeFields{ii} = placeFieldStats;
-    clear placeFieldStats
+    targetFile = dir('*placeFields.cellinfo.mat'); 
+    try load(targetFile.name);
+        projectSessionResults.placeFields{ii} = placeFieldStats;
+        clear placeFieldStats
+    catch
+        projectSessionResults.placeFields{ii} = NaN;
+    end
     
     % behavior 
-    behaviour = getSessionLinearize;
-    projectSessionResults.behavior{ii} = behaviour;
-    clear behaviour
+    targetFile = dir('*behavior.cellinfo.mat'); 
+    try load(targetFile.name);
+        projectSessionResults.behavior{ii} = behavior;
+        clear behaviour
+    catch
+        projectSessionResults.behavior{ii} = NaN;
+    end
     
-    % spikes
-    
+    if includeLFP
+        lfp = getLFP;
+        projectSessionResults.lfp{ii} = spikes;
+        clear spikes
+    end
 end
 
-behaviour = getSessionLinearize;
-psth_lReward = spikesPsth([behaviour.events.lReward],'numRep',50,'saveMat',false,'min_pulsesNumber',20,'winSize',5,'event_ints',[0 1],'winSizePlot',[-2 2]);
-
+save([analysis_project_path filesep date '_' project '.mat'],'projectSessionResults','-v7.3');
+%% stack all results
+projectResults.optogeneticResponses = stackSessionResult(projectSessionResults.optogeneticResponses);
 
 end
