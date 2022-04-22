9a68df11679f59caf9a2ecb33aaa9a04ad4a2d32
valegarman <valegarman@gmail.com>
2022-04-22 14:55:18 -0400

    update spikesPsth and loadProjectResults

---
+++ utilities/system/database_path.m
@@ -0,0 +1,15 @@
+function dp = database_path
+% return dropbox folder path
+    switch getenv('computername')
+        case 'MANULAPTOP'
+            dp = 'Z:\data';
+        case 'MANUPC'
+            dp = 'X:\data';
+        case 'MANUXPS'
+            dp = 'W:';
+        case 'SB13FLPC017'
+            %dp = 'W';
+            keyboard;
+    end
+
+end
