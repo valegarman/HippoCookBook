bb84228ac8bc7cca660ea3c5917c933e3773aed6
abadpablo <pab9525@gmail.com>
2022-08-04 13:01:09 -0400

    small fixes in sessionBatchScript

--- session_managment/personalFiles/sessionBatchScript_MV.m
+++ session_managment/personalFiles/sessionBatchScript_MV.m
@@ -5,15 +5,25 @@ updateExpFolder({'U:\fCr1', 'Y:\fCr1'},'X:\data\fCr1');
 
 updateExpFolder({'V:\data\fCck1', 'Y:\fCck1'},'E:\data\fCck1');
 
+updateExpFolder('V:\data\fId3','J:\fId3');
+
+
 %2% Preprocessing
 batch_preprocessSession('basepath','X:\data\fCr1','analysisPath','F:\fCr1','cleanArtifacts',({[],1}),'analogChannelsList',[],'digitalChannelsList',1);
 
 batch_preprocessSession('basepath','G:\data\fPv4','cleanArtifacts',({65,[]}),'analogChannelsList',65,'digitalChannelsList',0);
 
-batch_preprocessSession('basepath','D:\Dropbox\DATA\anna_data\NY14')
+batch_preprocessSession('basepath','D:\Dropbox\DATA\anna_data\NY14');
+
+batch_preprocessSession('basepath','J:\fId3','analysisPath','C:\data\fId3','cleanArtifacts',({[],1}),'analogChannelsList',[],'digitalChannelsList',1,'bazler_ttl_channel',[]);
+
+preprocessSession('basepath',pwd,'analogChannelsList',analogChannelsList,'spikeSort',spikeSort,'getPos',getPos, 'cleanArtifacts',cleanArtifacts,...
+                    'medianSubstr',medianSubstr,'tracking_pixel_cm',tracking_pixel_cm,'sessionSummary',sessionSummary,'digitalChannelsList',digitalChannelsList,'bazler_ttl_channel',bazler_ttl_channel);
+
+
 
 %3% Computing summary
-batch_sessionSummary('basepath','E:\data\fCck1','analogChannelsList',[],'digitalChannelsList',1);
+batch_sessionSummary('basepath','J:\fId3','analogChannelsList',[],'digitalChannelsList',1);
 
 batch_sessionSummary('basepath','G:\data\fNkx11','analogChannelsList',65,'digitalChannelsList',[]);
 
