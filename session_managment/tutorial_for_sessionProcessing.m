605ff56a298fd0fd4c163197022677cd847b0aeb
valegarman <valegarman@gmail.com>
2022-09-15 12:54:39 -0400

    small fixes

--- session_managment/tutorial_for_sessionProcessing.m
+++ session_managment/tutorial_for_sessionProcessing.m
@@ -20,10 +20,6 @@ batch_sessionSummary('basepath','G:\data\fPv4','cleanArtifacts',({65,[]}),'analo
 % 4% Processs individual sessions by by 'processSession'. Example:
 processSession('digital_optogenetic_channels',[],'analog_optogenetic_channels',1,'promt_hippo_layers',true,'manual_analog_pulses_threshold',false);
 
-processSession('digital_optogenetic_channels',[],'analog_optogenetic_channels',1,'promt_hippo_layers',true,'excludeAnalysis',{'3'});
-
-
-
 % 5% Index session
 indexNewSession;
 
