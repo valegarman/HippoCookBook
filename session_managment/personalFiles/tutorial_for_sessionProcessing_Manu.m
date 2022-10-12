1f9a337dcdc452039275ef3032e3c0e6581c9e44
valegarman <valegarman@gmail.com>
2022-10-12 17:55:13 -0400

    small fixes

--- session_managment/personalFiles/tutorial_for_sessionProcessing_Manu.m
+++ session_managment/personalFiles/tutorial_for_sessionProcessing_Manu.m
@@ -18,7 +18,7 @@ batch_sessionSummary('basepath','G:\data\fPv4','cleanArtifacts',({65,[]}),'analo
 % 3% CLEAN SESSIONS MANUALLY BY PHY
 
 % 4% Processs individual sessions by by 'processSession'. Example:
-processSession('digital_optogenetic_channels',1,'analog_optogenetic_channels',[],'promt_hippo_layers',true,'manual_analog_pulses_threshold',true,'profileType','hippocampus');
+processSession('digital_optogenetic_channels',1,'analog_optogenetic_channels',[],'promt_hippo_layers',true,'profileType','hippocampus');
 
 % 5% Index session
 indexNewSession;
