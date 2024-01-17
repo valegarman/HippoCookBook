3aa339d0251bb390cfc4edccb8b9c2087a1212b0
Picco - <mpicco@researchmar.net>
2024-01-17 18:17:04 +0100

    fix: remove wrong input in detectThetaEpoch line

--- utilities/others/ScriptBook_for_updatingLightInInh_sessions.m
+++ utilities/others/ScriptBook_for_updatingLightInInh_sessions.m
@@ -5,13 +5,13 @@ pulses = getAnalogPulses('manualThr',true,'force',true); % 1-index
 close all;
 getDigitalIn('force', true);
 uLEDPulses = getuLEDPulses('Current',3,'force',true,'ledLayout','ledLayoutScience2022');
+
 % important, in the Extracellular tab, update Electrode groups and Spike groups witht following info:
 % Group         Channel
 % 1             25 21 24 28 26 22 23 27
 % 2             29 17 20 32 30 18 19 31
 % 3             16 4 1 13 15 3 2 14
 % 4             12 8 5 9 11 7 6 10
-
 processSession('digital_optogenetic_channels',[11 12 13 14 15 16],'analog_optogenetic_channels',[3 4 5 6 7 8],...
     'promt_hippo_layers',true,'profileType','hippocampus','force_analogPulsesDetection',false,...
     'bazler_ttl_channel',1,'leftArmTtl_channel',3,'leftArmTtl_channel',4,'useCSD_for_theta_detection',false);
