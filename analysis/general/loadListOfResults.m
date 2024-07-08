683d7815d4f9969e0e86fe04c279c1b1638e5c7d
Manu <valegarman@gmail.com>
2024-07-08 18:43:52 +0200

    feat: loadProjectResult with loops acroos results

---
+++ analysis/general/loadListOfResults.m
@@ -0,0 +1,10 @@
+function list = loadListOfResults(project)
+
+list = [];
+switch lower(project)
+    case 'bibliocampus'
+        list = {'optogeneticResponse','averageCCG','ripples_psth','slowOscillations_psth','theta_*.PhaseLockingData','thetaREM*.PhaseLockingData',...
+            'thetaRun*.PhaseLockingData','lgamma*.PhaseLockingData','hgamma*.PhaseLockingData','ripple*.PhaseLockingData','spatialModulation','placeFields','behavior.cellinfo','ACGPeak',...
+            'speedCorr','uLEDResponse.cellinfo'};
+end
+end
