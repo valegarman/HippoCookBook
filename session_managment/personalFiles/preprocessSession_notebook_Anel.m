b33ad4ee05b69cd1e5513448c87098ae97329f74
anemarfel <anel.mmf@gmail.com>
2025-05-29 16:19:41 +0200

    Changes

--- session_managment/personalFiles/preprocessSession_notebook_Anel.m
+++ session_managment/personalFiles/preprocessSession_notebook_Anel.m
@@ -3,9 +3,9 @@ updateExpFolder_temp({'E:\app2'},'C:\app2');
 
 %preprocessSession('basepath','C:\app2\app2_250331_sess1','analysisPath',[],'cleanArtifacts',({[],[]}),'analogChannelsList',[],'digitalChannelsList',[],'sessionSummary',false,'getPos',false);
 
-processSession('basepath','C:\app2\app2_250401_sess2','digital_optogenetic_channels',[],'analog_optogenetic_channels',[],'promt_hippo_layers',true);
+processSession('basepath','C:\app2\app2_250409_sess8','digital_optogenetic_channels',[],'analog_optogenetic_channels',[],'promt_hippo_layers',true);
 
-indexNewSession('basepath','C:\app2\app2_250401_sess2');
+indexNewSession('basepath','C:\app2\app2_250409_sess8');
 
 %% WT5
 preprocessSession('basepath','C:\wt5\wt5_250331_sess1','analysisPath',[],'exclude_shanks',[3],'cleanArtifacts',({[],[]}),'analogChannelsList',[],'digitalChannelsList',[],'sessionSummary',false,'getPos',false);
