56bc5de6bb8c900dace94ec4d00e2ad4a84fe051
Gallardo Molina <agallardo@researchmar.net>
2026-05-21 11:03:44 +0200

    updating csv

--- session_managment/processSession.m
+++ session_managment/processSession.m
@@ -305,7 +305,7 @@ if ~any(ismember(excludeAnalysis, {'8',lower('eventsModulation')}))
     % 8.2 Ripples
 
 
-    ripples = rippleMasterDetector('rippleChannel',rippleChannel,'SWChannel',SWChannel,'force',true,'skipStimulationPeriods',true,'thresholds',rippleMasterDetector_threshold, 'eventSpikeThreshold',0.5,'referenceChannel', 30); 
+    ripples = rippleMasterDetector('rippleChannel',rippleChannel,'SWChannel',SWChannel,'force',true,'skipStimulationPeriods',true,'thresholds',rippleMasterDetector_threshold, 'eventSpikeThreshold',0.5,'excludeIntervals', ts); 
 
     psthRipples = spikesPsth([],'eventType','ripples','numRep',500,'force',true,'minNumberOfPulses',10);
     % psthRipples = spikesPsth([],'eventType','ripples','numRep',500,'force',true,'minNumberOfPulses',10,'restrict_to_manipulation',true);
